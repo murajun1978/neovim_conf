dc95c555b715fd50adb3649530e3cafe7585bf5d
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2024-05-19 01:51:03 +0900

    Add Neotree never show directories

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,14 @@ return {
           show_hidden_count = true,
           hide_dotfiles = false,
           hide_gitignored = false,
+          never_show_by_pattern = {
+            "*/vendor/bundle",
+          },
+          never_show = {
+            ".git",
+            ".ruby-lsp",
+            "node_modules",
+          },
         },
       },
     },
